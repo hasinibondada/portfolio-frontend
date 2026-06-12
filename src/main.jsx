13172534a23a6027d98bb15e1388663a34bd987a
HASINI BONDADA <hasinibondada25@gmail.com>
2026-06-12 13:40:29 +0530

    fix HashRouter closing tag

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <AuthProvider>
           <App />
         </AuthProvider>
-      </BrowserRouter>
+        </HashRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
